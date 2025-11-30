82ddb7709850fe9b1601e20a7c62336a17af884a
Pancholot <panchitofaller@gmail.com>
2025-11-30 03:41:59 -0600

    Add async_mode to SocketIO initialization for improved performance

--- extensions/__init__.py
+++ extensions/__init__.py
@@ -1,5 +1,5 @@
 from flask_socketio import SocketIO
 from flask_jwt_extended import JWTManager
 
-socketio = SocketIO(cors_allowed_origins="*")
+socketio = SocketIO(cors_allowed_origins="*", async_mode="eventlet")
 jwt = JWTManager()
